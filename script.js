dc1bf2bc100116623636e93e0a2497f931582fef
Alesya <alesiamokretsova@gmail.com>
2023-09-27 22:45:48 +0300

    Отрисовала тэги

--- script.js
+++ script.js
@@ -1,98 +1,163 @@
-const item = [{
+const items = [{
         title: "Гостиная пальма",
         description: "Подойдет для Вашего интерьера",
         tags: [],
         price: 78,
-        img: "./img/1.jpeg",
+        img: "./img/Palm.jpeg",
         rating: 4.4,
     },
     {
-        title: "Игрушка лабиринт",
-        description: "Поможет в развитии интеллекта!",
-        tags: ["cat", "dog"],
-        price: 900,
-        img: "./img/2.jpeg",
+        title: "Сансевиерия",
+        description: "Лучшее комнатное растение для очистки воздуха",
+        tags: [],
+        price: 72,
+        img: "./img/Sansev.jpeg",
         rating: 3.1,
     },
     {
-        title: "Игрушка для котят",
-        description: "Отвлечет вашего питомца!",
-        tags: ["cat"],
-        price: 300,
-        img: "./img/3.jpeg",
+        title: "Замиокулькас",
+        description: "Маленькая Африка у Вас дома!",
+        tags: [],
+        price: 98,
+        img: "./img/Zamiok.jpeg",
         rating: 5.0,
     },
     {
-        title: "Миска «Котик»",
-        description: "Подойдет и для собак!",
-        tags: ["cat", "dog"],
-        price: 660,
-        img: "./img/4.jpeg",
+        title: "Хамедорея изящная",
+        description: "Создаст небольшие тропики в вашем интерьере.",
+        tags: [],
+        price: 92,
+        img: "./img/Hamedorea.jpeg",
         rating: 4.7,
     },
     {
-        title: "Лоток розовый",
-        description: "Теперь вы можете забыть о проблемах с туалетом",
-        tags: ["cat"],
-        price: 400,
-        img: "./img/5.jpeg",
+        title: "Пахира водная",
+        description: "Теперь «ореховое дерево» и в Вашем доме! ",
+        tags: [],
+        price: 72,
+        img: "./img/Pahira.jpeg",
         rating: 4.9,
     },
     {
-        title: "Сухой корм для кошек",
-        description: "Специальная формула для милых усатиков!",
-        tags: ["cat"],
-        price: 200,
-        img: "./img/6.jpeg",
+        title: "Аспидистра элатиор",
+        description: "Стильное, простое в уходе декоративно-лиственное растение.",
+        tags: [],
+        price: 69,
+        img: "./img/Aspidiastra.jpeg",
         rating: 3.2,
     },
     {
-        title: "Сухой корм для собак",
-        description: "Содержит полный комплекс витаминов",
-        tags: ["dog"],
-        price: 300,
-        img: "./img/7.jpeg",
+        title: "Монстера минима",
+        description: "Стильное тропическое декоративно-лиственное растение.",
+        tags: [],
+        price: 55,
+        img: "./img/Monstera.jpeg",
         rating: 2.9,
     },
     {
-        title: "Игрушка для собак",
-        description: "Теперь вы можете не переживать за личные вещи",
-        tags: ["dog"],
-        price: 500,
-        img: "./img/8.jpeg",
+        title: "Калатея орбифолия",
+        description: "Яркое декоративно-лиственное растение.",
+        tags: [],
+        price: 58,
+        img: "./img/Kalateria.jpeg",
         rating: 3.4,
     },
     {
-        title: "Лежанка",
-        description: "Идеальное место для отдыха!",
-        tags: ["cat", "dog"],
-        price: 1500,
-        img: "./img/9.jpeg",
+        title: "Асплениум осака",
+        description: "Стильное достаточно неприхотливое растение.",
+        tags: [],
+        price: 77,
+        img: "./img/Asplenium.jpeg",
         rating: 4.8,
     },
     {
-        title: "Поилка для собак",
-        description: "Возьмите с собой в путешествие",
-        tags: ["dog"],
-        price: 800,
-        img: "./img/10.jpeg",
+        title: "Фикус лирата",
+        description: "Фикус Лирата Бамбино 12 - это стильное декоративно-лиственное растение. ",
+        tags: [],
+        price: 71,
+        img: "./img/Fikus.jpeg",
         rating: 3.2,
     },
     {
-        title: "Переноска",
-        description: "Путешествуйте с комфортом!",
-        tags: ["cat", "dog"],
-        price: 3500,
-        img: "./img/11.jpeg",
+        title: "Рипсалис",
+        description: "Уникальное ампельное суккулентное растение.",
+        tags: [],
+        price: 80,
+        img: "./img/Ripsalis.jpeg",
         rating: 3.7,
     },
     {
-        title: "Поводок для собак",
-        description: "Для чудесных прогулок вместе",
-        tags: ["dog"],
-        price: 800,
-        img: "./img/12.jpeg",
+        title: "Бегония макулата",
+        description: "Стильное редкое декоративно-лиственное растение.",
+        tags: [],
+        price: 63,
+        img: "./img/Begonia.jpeg",
         rating: 4.1,
     },
 
-]
+];
+
+let currentState = [...items];
+
+const itemsContainer = document.querySelector("#shop-items");
+const itemTemplate = document.querySelector("#item-template");
+const nothingFound = document.querySelector("#nothing-found");
+
+function renderItems(arr) {
+    nothingFound.textContent = "";
+    itemsContainer.innerHTML = "";
+    arr.forEach((item) => {
+        itemsContainer.append(prepareShopItem(item));
+    });
+
+    if (!arr.length) {
+        nothingFound.textContent = "Ничего не найдено"
+        ж
+    }
+}
+
+function sortByAlphabet(a, b) {
+    if (a.title > b.title) {
+        return 1;
+    }
+    if (a.title < b.title) {
+        return -1;
+    }
+    return 0;
+}
+
+renderItems(currentState.sort((a, b) => sortByAlphabet(a, b)));
+
+function prepareShopItem(shopItem) {
+    const { title, description, tags, img, price, rating } = shopItem;
+
+    const item = itemTemplate.content.cloneNode(true);
+
+    item.querySelector("h1").textContent = title;
+    item.querySelector("p").textContent = description;
+    item.querySelector("img").src = img;
+    item.querySelector(".price").textContent = `${price}$`;
+
+    const ratingContainer = item.querySelector(".rating");
+
+    for (let i = 0; i < rating; i++) {
+        const star = document.createElement("i");
+        star.classList.add("fa", "fa-star");
+        ratingContainer.append(star);
+    }
+
+    const tagsHolder = item.querySelector(".tags");
+
+    tags.forEach((tag) => {
+        const element = document.createElement("span");
+        element.textContent = tag;
+        element.classList.add("tag");
+        tagsHolder.append(element);
+    });
+
+    return item;
+
+}
+
+const searchInput = document.querySelector("#search-input");
+const searchButton = document.querySelector("#search-btn")
